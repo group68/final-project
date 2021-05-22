2b8d149ae83ba8528bd23ac268323038699a62d8
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2021-05-22 18:44:42 +0700

    adding dump database file

---
+++ Config/Application.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Config;
+
+/*
+ * This application config
+ */
+class Application {
+    const SHOW_ERRORS = true;
+    const DEFAULT_ROUTE = "/home";
+}
---
+++ Config/Database.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Config;
+
+/*
+ * This database config
+ */
+class Database {
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'restaurant';
+    const DB_USER = 'root';
+    const DB_PASSWORD = '';
+}
