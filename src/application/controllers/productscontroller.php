a30e2aed5d1abdae35f17b7175ad7e3386236ef0
erikha1501 <maskdroid01@gmail.com>
2021-05-27 09:22:11 +0700

    Add sample index action

--- src/application/controllers/productscontroller.php
+++ src/application/controllers/productscontroller.php
@@ -6,17 +6,22 @@ class ProductsController extends VanillaController
     {
     }
 
+    function index()
+    {
+        $products = $this->Product->custom("SELECT `product_id`, `NAME` FROM `products`");
+
+        $this->set_template_variable('products', $products);
+    }
+
     function view($id = null)
     {
         $product_id = $this->Product->sanitize($id);
-        $product = $this->Product->custom("SELECT * FROM `products` WHERE `product_id` = {$product_id}");
-        if (count($product) > 0) {
-            $product = $product[0];
+        $products = $this->Product->custom("SELECT * FROM `products` WHERE `product_id` = {$product_id}");
+        if (count($products) > 0) {
+            $this->set_template_variable('product', $products[0]);
         } else {
             
         }
-
-        $this->set_template_variable('product', $product);
     }
 
 
