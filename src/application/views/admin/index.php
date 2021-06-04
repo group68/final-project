8532eedde7bfba7008d509bbbff10eaf84362c14
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2021-06-04 13:54:13 +0700

    add admin view

--- src/application/views/admin/index.php
+++ src/application/views/admin/index.php
@@ -6,7 +6,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-  <link rel="stylesheet" href="src/application/views/admin/style.css">
+  <link rel="stylesheet" href="./style.css">
 
 </head>
 <!-- <style>
