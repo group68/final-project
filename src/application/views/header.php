afb255b880fdf2afb69a91cb982ff829e2ae1fba
Duy Nguyen <anhduy0911@gmail.com>
2021-06-04 14:13:45 +0700

    add header, footer

--- src/application/views/header.php
+++ src/application/views/header.php
@@ -1,13 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fast-food restaurant</title>
 
-    <?php echo $html->includeCss("index"); ?>
+    <?php echo $html->includeCss("header"); ?>
+    <?php echo $html->includeCss("footer"); ?>
+    <?php echo $html->includeCss("home"); ?>
+    <?php echo $html->includeJsDeffered("header"); ?>
 </head>
 
 <body>
-<div class="navigation"><h1>Fast-food restaurant</h1>
+    <header id="navbar">
+        <nav class="navbar-container container">
+            <a href="/" class="home-link">
+                <div class="navbar-logo"></div>
+                SE-FastFood
+            </a>
+            <button type="button" class="navbar-toggle" aria-label="Open navigation menu">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="navbar-menu">
+                <ul class="navbar-links">
+                    <li class="navbar-item"><a class="navbar-link" href="">Home</a></li>
+                    <li class="navbar-item"><a class="navbar-link" href="">Menu</a></li>
+                    <li class="navbar-item"><a class="navbar-link" href="">Order</a></li>
+                    <li class="navbar-item"><a class="navbar-link" href="">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
