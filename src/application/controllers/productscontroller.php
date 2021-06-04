4c8182b8f87769bb5ab75f9cc887c12b6a8de3b9
Duy Nguyen <anhduy0911@gmail.com>
2021-06-04 21:11:45 +0700

    thread index -> category -> detail item

--- src/application/controllers/productscontroller.php
+++ src/application/controllers/productscontroller.php
@@ -4,14 +4,15 @@ class ProductsController extends VanillaController
 {
     function index()
     {
-        $products = $this->Product->custom("SELECT `product_id`, `NAME` FROM `products`");
-
-        if (!$products) {
+        // $products = $this->Product->custom("SELECT `product_id`, `NAME` FROM `products`");
+        $categories = $this->Product->categories;
+        $imgs = $this->Product->getCategoryImg();
+        if (!$categories) {
             return false;
         }
 
-        $this->set_template_variable('products', $products);
-
+        $this->set_template_variable('categories', $categories);
+        $this->set_template_variable('imgs', $imgs);
         return true;
     }
 
@@ -26,6 +27,24 @@ class ProductsController extends VanillaController
 
         if (count($products) > 0) {
             $this->set_template_variable('product', $products[0]);
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+    function category($id = null)
+    {
+        $cate_id = $this->Product->sanitize($id);
+        $products = $this->Product->custom("SELECT * FROM `products` WHERE `category` = {$cate_id}");
+
+        if (!$products) {
+            return false;
+        }
+
+        if (count($products) > 0) {
+            $this->set_template_variable('products', $products);
         } else {
             return false;
         }
