94b591cfd86750eb035283067b0a0731cb6c2fc9
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2021-06-08 01:43:29 +0700

    fix: UI at orderDetail and order Process

--- src/application/views/employees/processOrder.php
+++ src/application/views/employees/processOrder.php
@@ -1,5 +1,7 @@
+<?php echo $html->includeCss("orders"); ?>
 <div class="body-container p-base">
-    <div class="row">
+    <div class="order-container">
+    <div class="row text-center">
         <h2>All orders you've not processed</h2>
         <p>
             Let check the orders and accept if all the products are available.
@@ -7,7 +9,7 @@
     </div>
     <div>
         <form method="POST">
-            <table id="customers" class="plr-15 mt-15 shadow-box">
+            <table id="orders" class="plr-15 mt-15 shadow-box">
                 <tr>
                     <th>OK</th>
                     <th>OrderID</th>
@@ -21,26 +23,28 @@
 
                         $current = $order['Order'];
                         echo "<tr>";
-                        echo "<td><input type='checkbox' value='<?echo {$current["order_id"]}?>' name='orders[]'></td>";
-                        echo "<td>{$current['order_id']}</td>";
-                        echo "<td>{$current['customer_id']}</td>";
-                        echo "<td>{$current['created_at']}</td>";
-                        echo "<td>";
-                        echo "<a href='/employees/orderDetail/{$current['order_id']}'>";
+                        echo "<td><input type='checkbox' value='<?echo {$current["order_id"]}?>' name='orders[]'></td>
+                ";
+                echo "<td>{$current['order_id']}</td>";
+                echo "<td>{$current['customer_id']}</td>";
+                echo "<td>{$current['created_at']}</td>";
+                echo "<td class='see-more'>";
+                    echo "<a href='/employees/orderDetail/{$current['order_id']}'>";
                         echo "See more";
                         echo "</a>";
-                        echo "</td>";
-                        echo "</tr>";
-                        // echo "id: " . $current['order_id'] . " cus_id: " . $current['customer_id'] . " time: " .
+                    echo "</td>";
+                echo "</tr>";
+                // echo "id: " . $current['order_id'] . " cus_id: " . $current['customer_id'] . " time: " .
                 ?>
-                    <?php endforeach ?>
-                    <br><input type="submit" value="Process">
-                    <input type="reset" value="Reset" class="btn btn-secondary">
+                <?php endforeach ?>
+                
                 <? } else {
                     echo "You currently have no order assigned!\n";
                 } ?>
             </table>
-
+            <br><input type="submit" value="Process">
+                <input type="reset" value="Reset" class="btn btn-secondary">
         </form>
     </div>
 </div>
+            </div>
