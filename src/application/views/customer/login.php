e1189421e82c1fc47fe5bbe4dda92fce45efa24e
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2021-06-08 01:03:07 +0700

    fix: UI at login views

--- src/application/views/customer/login.php
+++ src/application/views/customer/login.php
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-<div style="padding: 15%;">
-    <form action="/customer/login" method="post">
-        <h1>Login form</h1>
-        <br>
-        <p class="highlight">Please enter your username and password to continue</p>
-        <br>Username: <input text type="text" size="50" maxlength="50" minlength="3" name="username">
-        <br>Password: <input text type="password" size="30" maxlength="30" minlength="3" name="password">
-        <br />
-        <br />
-        <input type="submit" value="Login">
-        <input type="reset" value="Reset">
-        <br />
-        <br />
-        <a href="/customer/register">Don't have an account? Click here to join with us!</a>
-        <? if (isset($err)) echo $err; ?>
-    </form>
-</div>
-=======
 <div class="body-container large-p-base">
     <div class="shadow-box p-base round-border">
         <form action="/customer/login" method="post">
@@ -36,4 +17,3 @@
         </form>
     </div>
 </div>
->>>>>>> 2fd3358 (fix: UI at customers and products views)
