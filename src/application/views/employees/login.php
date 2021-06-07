17c3b1836a19cf583f51be3551432e9c5060de2f
Thuy Dung Nguyen <thuydungnguyen.ictk63@gmail.com>
2021-06-07 20:48:12 +0700

    fix: UI at employee-side

--- src/application/views/employees/login.php
+++ src/application/views/employees/login.php
@@ -1,16 +1,17 @@
-<div style="padding: 5%;">
-<div>
-    <form action="/employees/login" method="post">
-        <br>
-        <p class="highlight">Please enter your username and password to continue</p>
-        <p class="highlight">Employee Login</p>
-        <br>Username: <input text type="text" size="50" maxlength="50" name="username">
-        <br>
-        Password: <input text type="password" size="30" maxlength="30" name="password"><br>
-        <br>
-        <input type="submit" value="Login">
-        <input type="reset" value="Reset">
-        <br>
-        <? if (isset($err)) echo $err; ?>
-    </form>
+<div class="row body-container shadow-box p-base bg-blur round-border">
+    <div>
+        <form action="/employees/login" method="post">
+            <br>
+            <p class="highlight">Please enter your username and password to continue</p>
+            <h2 class="highlight">Employee Login</h2>
+            <br>Username: <input text type="text" size="50" maxlength="50" name="username">
+            <br>
+            Password: <input text type="password" size="30" maxlength="30" name="password"><br>
+            <br>
+            <input type="submit" value="Login">
+            <input type="reset" value="Reset" class="btn btn-secondary">
+            <br>
+            <? if (isset($err)) echo $err; ?>
+        </form>
+    </div>
 </div>
--- src/application/views/employees/processOrder.php
+++ src/application/views/employees/processOrder.php
@@ -1,20 +1,40 @@
-<div style="padding: 5%;">
-<div>
-
-<form method="POST">
-    <? 
+<div class="body-container p-base">
+    <div class="row">
+        <h2>All orders you've not processed</h2>
+        <p>
+            Let check the orders and accept if all the products are available.
+        </p>
+    </div>
+    <div>
+        <form method="POST">
+            <table id="customers" class="plr-15 mt-15 shadow-box">
+                <tr>
+                    <th>OK</th>
+                    <th>OrderID</th>
+                    <th>CustomerID</th>
+                    <th>Created at</th>
+                </tr>
+                <?
     if ($orders) {
         foreach ($orders as $order) :
 
             $current = $order['Order'];
-            echo "id: " . $current['order_id']  . " cus_id: " . $current['customer_id'] . " time: " . $current['created_at'];
-    ?>
-    <br><input type="checkbox" value="<?echo $current['order_id']?>" name="orders[]">
-        <? endforeach ?>
-        <br><input type="submit" value="Process orders">
-        <input type="reset" value="Reset">
-    <? } else {
+            echo "<tr>";
+            echo "<td><input type='checkbox' value='<?echo {$current["order_id"]}?>' name='orders[]'></td>";
+                echo "<td>{$current['order_id']}</td>";
+                echo "<td>{$current['customer_id']}</td>";
+                echo "<td>{$current['created_at']}</td>";
+                echo "</tr>";
+                // echo "id: " . $current['order_id'] . " cus_id: " . $current['customer_id'] . " time: " .
+                ?>
+                <?php endforeach ?>
+            </table>
+            <br><input type="submit" value="Process">
+            <input type="reset" value="Reset" class="btn btn-secondary">
+            <? } else {
         echo "no order!\n";
     } ?>
-</form>
+
+        </form>
+    </div>
 </div>
